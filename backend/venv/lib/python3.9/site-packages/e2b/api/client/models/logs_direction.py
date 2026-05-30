3c3d161219dbdf325ba281afc6113b229546af16
Pari Agarwal <pariagarwal@Paris-MacBook-Air.local>
2026-05-30 21:15:47 +0530

    Remove venv and .env from repository

--- backend/venv/lib/python3.9/site-packages/e2b/api/client/models/logs_direction.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class LogsDirection(str, Enum):
-    BACKWARD = "backward"
-    FORWARD = "forward"
-
-    def __str__(self) -> str:
-        return str(self.value)
--- backend/venv/lib/python3.9/site-packages/e2b/api/client/models/logs_source.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class LogsSource(str, Enum):
-    PERSISTENT = "persistent"
-    TEMPORARY = "temporary"
-
-    def __str__(self) -> str:
-        return str(self.value)
